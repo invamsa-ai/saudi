c34b51f24923bab9901ba60bba3a88a7c2f080c6
invamsa-ai <invamsa@gmail.com>
2025-12-31 02:23:36 +0300

    Create firebase-config.js
    Signed-off-by: invamsa-ai <invamsa@gmail.com>

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyBxYOjHfK7D6J4v7pYf9JqZ8lM9nT3cVwX",
+    authDomain: "your-project-id.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project-id.appspot.com",
+    messagingSenderId: "123456789012",
+    appId: "1:123456789012:web:abcdef1234567890abcdef"
+};
