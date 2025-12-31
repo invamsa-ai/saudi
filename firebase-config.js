71af37d6bf488398d361e1cd9c5cd769194a8346
invamsa-ai <invamsa@gmail.com>
2025-12-31 07:30:00 +0300

    Update firebase-config.js
    Signed-off-by: invamsa-ai <invamsa@gmail.com>

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
-// firebase-config.js
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBxYOjHfK7D6J4v7pYf9JqZ8lM9nT3cVwX",
-    authDomain: "your-project-id.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project-id.appspot.com",
-    messagingSenderId: "123456789012",
-    appId: "1:123456789012:web:abcdef1234567890abcdef"
+  apiKey: "AIzaSyA1c9ft1XJrmjKIwhdxixJ1KM5Q3VL9HvQ",
+  authDomain: "uaewep-38378.firebaseapp.com",
+  projectId: "uaewep-38378",
+  storageBucket: "uaewep-38378.firebasestorage.app",
+  messagingSenderId: "366757774392",
+  appId: "1:366757774392:web:32da696c806184709ed2cd",
+  measurementId: "G-XNZC0GHCMM"
 };
